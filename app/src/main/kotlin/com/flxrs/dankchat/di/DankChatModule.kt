ff3eebde17c3a487752c3803c2a1c94b7d78cbc1
Felix <felixschmidbb@gmail.com>
2026-03-29 21:04:29 +0200

    style: Remove stale comment from DankChatModule

--- app/src/main/kotlin/com/flxrs/dankchat/di/DankChatModule.kt
+++ app/src/main/kotlin/com/flxrs/dankchat/di/DankChatModule.kt
@@ -5,4 +5,4 @@ import org.koin.core.annotation.Module
 
 @Module(includes = [ConnectionModule::class, DatabaseModule::class, NetworkModule::class, CoroutineModule::class])
 @ComponentScan("com.flxrs.dankchat")
-class DankChatModule // ksp regen v3
+class DankChatModule
