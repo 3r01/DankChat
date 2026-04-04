17d8f15a6ee64aeac033f0ed8d78b1dab65aa97f
Felix <felixschmidbb@gmail.com>
2026-04-04 11:53:51 +0200

    chore: Bump version to 4.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.flxrs.dankchat"
         minSdk = 30
         targetSdk = 35
-        versionCode = 40010
-        versionName = "4.0.10"
+        versionCode = 40011
+        versionName = "4.0.11"
     }
 
     androidResources { generateLocaleConfig = true }
